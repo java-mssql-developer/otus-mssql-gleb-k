6d907fd0ffdab37c21b10da51bdef6df038b9a77
Gleb <kaacjavacpp@web.de>
2022-09-28 00:26:18 +0200

    homework 2, questions 1,2

---
+++ hw2/2.sql
@@ -0,0 +1,9 @@
+﻿/****** Поставщиков (Suppliers), у которых не было сделано ни одного заказа (PurchaseOrders).  ******/
+SELECT TOP (1000) s.[SupplierID]
+			       ,[SupplierName]
+				   ,p.PurchaseOrderID
+     
+  FROM [WideWorldImporters].[Purchasing].[Suppliers] s
+  FULL OUTER JOIN  [WideWorldImporters].[Purchasing].[PurchaseOrders] p
+  ON s.SupplierID=p.SupplierID
+  where p.PurchaseOrderID IS NULL
