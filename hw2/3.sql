3b7158243321dcc098f5c98d6e6c588f310e59b8
Gleb <kaacjavacpp@web.de>
2022-10-05 21:05:31 +0200

    обновление 3/6

--- hw2/3.sql
+++ hw2/3.sql
@@ -5,5 +5,5 @@ select o.OrderID, o.PickingCompletedWhen, ol.UnitPrice, ol.Quantity
 from Sales.Orders o
 inner join Sales.OrderLines ol
 on o.OrderID=ol.OrderID
-where ol.UnitPrice>'100' OR ol.Quantity>'20' AND o.PickingCompletedWhen<>null
+where (ol.UnitPrice>'100' OR ol.Quantity>'20') AND o.PickingCompletedWhen is not null
 
--- hw2/6.sql
+++ hw2/6.sql
@@ -1,20 +1,13 @@
 ﻿--Все ид и имена клиентов и их контактные телефоны, которые покупали товар "Chocolate frogs 250g".--
 use WideWorldImporters
 go
-with OrdersCustomers as (
-select o.CustomerID, c.CustomerName, o.OrderID, c.PhoneNumber
-from Sales.Customers c
-join Sales.Orders o
+
+select o.CustomerID, c.CustomerName, c.PhoneNumber, s.StockItemName
+from Sales.Orders o
+join Sales.Customers c
 on o.CustomerID=c.CustomerID
-),
-
-ItemCustomer as (select distinct ol.StockItemID, oc.CustomerID, oc.CustomerName, oc.PhoneNumber, ol.OrderID 
-from OrdersCustomers oc
 join Sales.OrderLines ol
-on ol.OrderID=oc.OrderID)
-
-select ic.CustomerID, ic.CustomerName, ic.PhoneNumber
-from ItemCustomer ic
-join Warehouse.StockItems si
-on ic.StockItemID=si.StockItemID
-where si.StockItemName='Chocolate frogs 250g'
+on ol.OrderID=o.OrderID
+join Warehouse.StockItems s
+on s.StockItemID=ol.StockItemID
+where s.StockItemName='Chocolate frogs 250g'
